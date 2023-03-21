a7359611da7ed0a8f67f96f5d3625509ce426a06
gramakant <128539092+gramakant@users.noreply.github.com>
2023-03-21 16:50:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.subheader('Omega 3 & Blueberry Oatmeal'
+streamlit.subheader('Omega 3 & Blueberry Oatmeal')
 
+streamlit.text('kale, Spinach & Rocket Smoothie')
